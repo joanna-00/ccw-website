037b8b711afe13f34b0b044044e82b4cd504a005
joanna-00 <1081578@ucn.dk>
2020-06-06 16:27:02 +0200

    Shop CMS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,14 +24,15 @@ const catMembership = 29;
 const catService = 30;
 const tagBenefit = 25;
 
-function requestWP(endpoint, id) {
+function requestWP(endpoint, id, callback) {
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       let data = JSON.parse(this.responseText).reverse();
       data = acfParser(data);
       console.log(data);
-      return data;
+      callback(data);
+      // return data;
     }
   };
   xhttp.open("GET", `${apiUrl}posts${endpoint}${id}&per_page=100`, true);
@@ -49,48 +50,69 @@ function acfParser(object) {
   return array;
 }
 
-const shopItems = requestWP(catEndPoint, catService);
+function parseFilter(string) {
+  return string.split(":")[0];
+}
 
-function renderShop(data, outputContainer, filters) {
-  data.forEach((service) => {
-    `<div class="col-4">
-    <div
-      class="card card--image card--button card--description card--price"
-    >
-      <div class="card__image">
-        <img
-          src="../../assets/images/photos/steering-wheel-wash.jpg"
-          alt=""
-        />
+const shopItems = requestWP(catEndPoint, catService, renderShop);
+
+// renderShop(shopItems);
+
+function renderShop(shopItems) {
+  let exteriorContainer = $(".shop main #sectionExterior"),
+    interiorContainer = $(".shop main #sectionInterior"),
+    bundlesContainer = $(".shop main #sectionBundles"),
+    membershipsContainer = $(".shop main #sectionMemberships");
+  let shopContainer = $(".shop main");
+
+  shopItems.forEach((item) => {
+    // console.log(item);
+    let filters = [];
+    item.service_type_filter.forEach((filter) => {
+      filters.push(parseFilter(filter));
+    });
+    console.log(filters);
+    exteriorContainer.innerHTML += createShopCard(item);
+  });
+}
+
+function createShopCard(shopItem) {
+  return `<div class="col-12 col-lg-6 col-xl-4">
+  <div
+    class="card card--image card--button card--description card--price card--clickable"
+  >
+    <div class="card__image">
+      <img
+        src="${shopItem.imageafter}"
+        alt=""
+      />
+    </div>
+    <div class="card__content">
+      <div class="card__label">
+        <h4 class="card__title">
+        ${shopItem.title}
+        </h4>
+        <h4>
+          <a href="" class="card__link"
+            ><i class="fas fa-arrow-right"></i
+          ></a>
+        </h4>
       </div>
-      <div class="card__content">
-        <div class="card__label">
-          <h4 class="card__title">
-            Card title
-          </h4>
-          <h4>
-            <a href="" class="card__link"
-              ><i class="fas fa-arrow-right"></i
-            ></a>
-          </h4>
-        </div>
 
-        <p class="card__description">
-          card teext text textcard teext text textcard teext text
-          textcard teext text textcard teext text text
-        </p>
+      <p class="description_short">
+        ${shopItem.description_short}
+      </p>
 
-        <div class="card__features">
-          <h4 class="card__price">175</h4>
-          <button
-            class="card__button button button--base-lg button--primary-light"
-          >
-            Add to cart
-            <i class="fas fa-cart-plus"></i>
-          </button>
-        </div>
+      <div class="card__features">
+        <h4 class="card__price">${shopItem.price}</h4>
+        <button
+          class="card__button button button--base-lg button--primary-light"
+        >
+          Add to cart
+          <i class="fas fa-cart-plus"></i>
+        </button>
       </div>
     </div>
-  </div>`;
-  });
+  </div>
+</div>`;
 }
