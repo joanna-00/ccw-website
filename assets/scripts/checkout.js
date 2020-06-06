f2b90f00cf192a0ec33a84a7472856ec80dfbbf8
joanna-00 <1081578@ucn.dk>
2020-06-06 14:34:14 +0200

    Process steps

--- assets/scripts/checkout.js
+++ assets/scripts/checkout.js
@@ -1,14 +1,16 @@
-let currentStep = 3;
+let currentStep = 2;
 const processSteps = $$(".process-steps__step");
 
-// for (let i = 0; i < currentStep; i++) {
-processSteps.forEach((step) => {
-  if (step.dataset.step <= currentStep) {
-    step.getElementsByTagName("i")[0].remove();
-    let checkmark = document.createElement("i");
-    checkmark.setAttribute("class", "fas fa-check-circle completed");
-    // step.appendChild(checkmark);
-    step.insertBefore(checkmark, step.firstChild);
-  }
-});
-// }
+function updateStepIcons() {
+  processSteps.forEach((step) => {
+    if (step.dataset.step <= currentStep) {
+      step.getElementsByTagName("i")[0].remove();
+      let checkmark = document.createElement("i");
+      checkmark.setAttribute("class", "fas fa-check-circle completed");
+      // step.appendChild(checkmark);
+      step.insertBefore(checkmark, step.firstChild);
+    }
+  });
+}
+
+updateStepIcons();
