f9882bcac6aefdba0f0c3df2c44d310b5f7ac5eb
joanna-00 <1081578@ucn.dk>
2020-06-07 22:58:51 +0200

    Shop CMS filtering

--- assets/scripts/shop.js
+++ assets/scripts/shop.js
@@ -10,24 +10,68 @@ function renderShop(shopItems) {
   let shopContainer = $(".shop main");
 
   shopItems.forEach((item) => {
-    // console.log(item);
-    let filters = [];
-    item.acf.service_type_filter.forEach((filter) => {
-      filters.push(parseFilter(filter));
-    });
-    console.log(filters);
-    exteriorContainer.innerHTML += createShopCard(item);
+    // let filters = [];
+    // item.acf.service_type_filter.forEach((filter) => {
+    //   filters.push(parseFilter(filter));
+    // });
+    // console.log(filters);
+    let filter = item.acf.service_type_filter[0];
+    // console.log(filter);
+    filter = parseFilter(filter);
+
+    switch (filter) {
+      case "memberships":
+        membershipsContainer.innerHTML += createMembershipCard(item);
+        console.log(filter);
+        break;
+      case "exterior":
+        exteriorContainer.innerHTML += createShopCard(item);
+        break;
+      case "interior":
+        interiorContainer.innerHTML += createShopCard(item);
+        break;
+      case "bundles":
+        bundlesContainer.innerHTML += createShopCard(item);
+        break;
+      default:
+      // code block
+    }
+    if (filter === "memberships") {
+    }
   });
 }
 
+function createMembershipCard(shopItem) {
+  return `        <div class="col-3">
+  <div class="card card--membership">
+    <div class="card__image">
+      <img src="../../assets/images/svgs/check-24px.svg" alt="" />
+    </div>
+    <div class="card__content">
+      <h4 class="card__title">Platinum</h4>
+      <p class="card__description">
+        The quick, brown fox jumps over a lazy dog. DJs flock by when
+        MTV
+      </p>
+      <button
+        class="button button--base-lg button--secondary button--filled"
+      >
+        Buy now
+      </button>
+    </div>
+  </div>
+</div>`;
+}
+
 function createShopCard(shopItem) {
+  shopItem = shopItem.acf;
   return `<div class="col-12 col-lg-6 col-xl-4">
   <div
     class="card card--image card--button card--description card--price card--clickable"
   >
     <div class="card__image">
       <img
-        src="${shopItem.imageafter}"
+        src="${shopItem.thumbnail_image.url}"
         alt=""
       />
     </div>
