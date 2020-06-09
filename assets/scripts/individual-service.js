7be3a5ab6d8f8a11a9f6420cfda8305adbe78d33
NikolayR21 <1081601@ucn.dk>
2020-06-09 23:51:51 +0200

    .

--- assets/scripts/individual-service.js
+++ assets/scripts/individual-service.js
@@ -1,43 +1,55 @@
-const redirectServiceID = localStorage.getItem("redirectServiceID");
+if (window.location.pathname.includes("individual_service.html")) {
+  const redirectServiceID = localStorage.getItem("redirectServiceID");
 
-const individualService = requestPostWP(
-  postEndPoint,
-  redirectServiceID,
-  renderIndividualService
-);
+  const individualService = requestPostWP(
+    postEndPoint,
+    redirectServiceID,
+    renderIndividualService
+  );
 
-//  Individual page rendering
+  //  Individual page rendering
 
-function renderIndividualService(individualService) {
-  let id = individualService.id;
-  individualService = individualService.acf;
-  console.log("rendering page");
-  $(".breadcrumbs__item--active").textContent = individualService.title;
-  let descriptions = $$(".main-service__info__description-text p");
-  descriptions[0].textContent = individualService.description_long_text1;
-  descriptions[1].textContent = individualService.description_long_text2;
-  descriptions[2].textContent = individualService.description_long_text3;
-  let titles = $$(".main-service__info__description-text h3");
-  titles[0].textContent = individualService.description_long_title1;
-  titles[1].textContent = individualService.description_long_title2;
-  titles[2].textContent = individualService.description_long_title3;
-  $(".service-info-card__title").textContent = individualService.title;
-  $(".service-info-card__price-value").textContent = individualService.price;
-  $(
-    ".service-info-card__time-value"
-  ).textContent = `${individualService.est_time} - ${individualService.est_time_max} hr`;
-  $(
-    ".main-service__info__image"
-  ).style.backgroundImage = `url(${individualService.imageafter})`;
-  // return ``;
+  function renderIndividualService(individualService) {
+    let id = individualService.id;
+    individualService = individualService.acf;
+    console.log("rendering page");
+    $(".breadcrumbs__item--active a").textContent = individualService.title;
 
-  $(".service-info-card__button-cart").dataset.id = id;
-  $(".service-info-card__button-cart").addEventListener("click", () => {
-    addToShoppingCart(event);
-  });
-  $(".service-info-card__button-buy").dataset.id = id;
-  $(".service-info-card__button-buy").addEventListener("click", () => {
-    addToShoppingCart(event);
-    location.href = "shopping_cart.html";
-  });
+    let descriptions = $$(".main-service__info__description-text p");
+    descriptions[0].textContent = individualService.description_long_text1;
+    descriptions[1].textContent = individualService.description_long_text2;
+    descriptions[2].textContent = individualService.description_long_text3;
+
+    let titles = $$(".main-service__info__description-text h3");
+    titles[0].textContent = individualService.description_long_title1;
+    titles[1].textContent = individualService.description_long_title2;
+    titles[2].textContent = individualService.description_long_title3;
+
+    $(".service-info-card__title").textContent = individualService.title;
+    $(".service-info-card__price-value").textContent = individualService.price;
+    $(
+      ".service-info-card__time-value"
+    ).textContent = `${individualService.est_time} - ${individualService.est_time_max} hr`;
+    $(
+      ".main-service__info__image"
+    ).style.backgroundImage = `url(${individualService.imageafter})`;
+
+    $(".service-info-card__button-cart").dataset.id = id;
+    $(".service-info-card__button-cart").addEventListener("click", () => {
+      addToShoppingCart(event);
+    });
+    $(".service-info-card__button-buy").dataset.id = id;
+    $(".service-info-card__button-buy").addEventListener("click", () => {
+      addToShoppingCart(event);
+      location.href = "shopping_cart.html";
+    });
+
+    const reccomendedServices = requestPostWP(
+      catEndPoint,
+      catMembership,
+      renderHomepageMemberships
+    );
+  }
 }
+
+function renderReccomendedServices(reccomendedServices) {}
