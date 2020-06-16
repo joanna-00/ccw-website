a2db93067fe00f7dfb5882dfe2cd96cd6e893819
joanna-00 <1081578@ucn.dk>
2020-06-16 18:21:46 +0200

    Date picker class

--- assets/scripts/date-picker.js
+++ assets/scripts/date-picker.js
@@ -3,7 +3,7 @@ class DatePicker {
     this.date_picker_element = this.createNewDatePicker();
     this.dates_element = this.date_picker_element.querySelector(".dates");
     this.mth_element = this.date_picker_element.querySelector(
-      ".dates .month .next-mth"
+      ".dates .month .mth"
     );
     this.next_mth_element = this.date_picker_element.querySelector(
       ".dates .month .next-mth"
@@ -63,10 +63,14 @@ class DatePicker {
 
   setUpDatePicker() {
     // console.log(this);
+    let goToNextMonth = this.goToNextMonth.bind(this);
+    let goToPreviousMonth = this.goToPreviousMonth.bind(this);
+    // console.log(goToNextMonth);
     this.mth_element.textContent = this.months[this.month] + " " + this.year;
     this.date_picker_element.dataset.type = "checkout";
-    this.next_mth_element.addEventListener("click", this.goToNextMonth);
-    this.prev_mth_element.addEventListener("click", this.goToPreviousMonth);
+    this.next_mth_element.addEventListener("click", goToNextMonth);
+
+    this.prev_mth_element.addEventListener("click", goToPreviousMonth);
 
     this.populateDays();
   }
@@ -77,8 +81,9 @@ class DatePicker {
       this.month = 0;
       this.year++;
     }
+    console.log(this);
     this.mth_element.textContent = this.months[this.month] + " " + this.year;
-    populateDays();
+    this.populateDays();
   }
 
   goToPreviousMonth() {
@@ -87,6 +92,7 @@ class DatePicker {
       this.month = 11;
       this.year--;
     }
+    console.log(this);
     this.mth_element.textContent = this.months[this.month] + " " + this.year;
     populateDays();
   }
@@ -129,7 +135,7 @@ class DatePicker {
     `;
 
     let firstDayOfMonth = new Date(this.year, this.month, 1).getDay() || 7;
-    appendEmptyDays(firstDayOfMonth);
+    this.appendEmptyDays(firstDayOfMonth);
 
     let amount_days;
     switch (this.months[this.month]) {
@@ -272,159 +278,159 @@ class DatePicker {
 
 // populateDays();
 
-function customizeDatePickerEvent(callback) {
-  $$(".day_element").forEach((day) => {
-    day.addEventListener("click", callback);
-  });
-}
+// function customizeDatePickerEvent(callback) {
+//   $$(".day_element").forEach((day) => {
+//     day.addEventListener("click", callback);
+//   });
+// }
 
-function goToNextMonth() {
-  month++;
-  if (month > 11) {
-    month = 0;
-    year++;
-  }
-  mth_element.textContent = months[month] + " " + year;
-  populateDays();
-}
+// function goToNextMonth() {
+//   month++;
+//   if (month > 11) {
+//     month = 0;
+//     year++;
+//   }
+//   mth_element.textContent = months[month] + " " + year;
+//   populateDays();
+// }
 
-function goToPreviousMonth() {
-  month--;
-  if (month < 0) {
-    month = 11;
-    year--;
-  }
-  mth_element.textContent = months[month] + " " + year;
-  populateDays();
-}
+// function goToPreviousMonth() {
+//   month--;
+//   if (month < 0) {
+//     month = 11;
+//     year--;
+//   }
+//   mth_element.textContent = months[month] + " " + year;
+//   populateDays();
+// }
 
-function appendEmptyDays(firstDayOfMonth) {
-  for (let i = 1; i < firstDayOfMonth; i++) {
-    const empty_day_element = document.createElement("div");
-    empty_day_element.classList.add("day");
+// function appendEmptyDays(firstDayOfMonth) {
+//   for (let i = 1; i < firstDayOfMonth; i++) {
+//     const empty_day_element = document.createElement("div");
+//     empty_day_element.classList.add("day");
 
-    days_element.appendChild(empty_day_element);
-  }
-}
+//     days_element.appendChild(empty_day_element);
+//   }
+// }
 
-function isLeapYear(year) {
-  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-}
+// function isLeapYear(year) {
+//   return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+// }
 
-function populateDays() {
-  days_element.innerHTML = `                
-  <div class="day-week">M</div>
-  <div class="day-week">T</div>
-  <div class="day-week">W</div>
-  <div class="day-week">T</div>
-  <div class="day-week">F</div>
-  <div class="day-week">S</div>
-  <div class="day-week">S</div>
-  `;
+// function populateDays() {
+//   days_element.innerHTML = `
+//   <div class="day-week">M</div>
+//   <div class="day-week">T</div>
+//   <div class="day-week">W</div>
+//   <div class="day-week">T</div>
+//   <div class="day-week">F</div>
+//   <div class="day-week">S</div>
+//   <div class="day-week">S</div>
+//   `;
 
-  let firstDayOfMonth = new Date(year, month, 1).getDay() || 7;
-  appendEmptyDays(firstDayOfMonth);
+//   let firstDayOfMonth = new Date(year, month, 1).getDay() || 7;
+//   appendEmptyDays(firstDayOfMonth);
 
-  let amount_days;
-  switch (months[month]) {
-    case "January":
-      amount_days = 31;
-      break;
-    case "February":
-      if (isLeapYear(year)) {
-        amount_days = 29;
-      } else {
-        amount_days = 28;
-      }
-      break;
-    case "March":
-      amount_days = 31;
-      break;
-    case "April":
-      amount_days = 30;
-      break;
-    case "May":
-      amount_days = 31;
-      break;
-    case "June":
-      amount_days = 30;
-      break;
-    case "July":
-      amount_days = 31;
-      break;
-    case "August":
-      amount_days = 31;
-      break;
-    case "September":
-      amount_days = 30;
-      break;
-    case "October":
-      amount_days = 31;
-      break;
-    case "November":
-      amount_days = 30;
-      break;
-    case "December":
-      amount_days = 31;
-      break;
-  }
+//   let amount_days;
+//   switch (months[month]) {
+//     case "January":
+//       amount_days = 31;
+//       break;
+//     case "February":
+//       if (isLeapYear(year)) {
+//         amount_days = 29;
+//       } else {
+//         amount_days = 28;
+//       }
+//       break;
+//     case "March":
+//       amount_days = 31;
+//       break;
+//     case "April":
+//       amount_days = 30;
+//       break;
+//     case "May":
+//       amount_days = 31;
+//       break;
+//     case "June":
+//       amount_days = 30;
+//       break;
+//     case "July":
+//       amount_days = 31;
+//       break;
+//     case "August":
+//       amount_days = 31;
+//       break;
+//     case "September":
+//       amount_days = 30;
+//       break;
+//     case "October":
+//       amount_days = 31;
+//       break;
+//     case "November":
+//       amount_days = 30;
+//       break;
+//     case "December":
+//       amount_days = 31;
+//       break;
+//   }
 
-  for (let i = 0; i < amount_days; i++) {
-    const day_element = document.createElement("div");
-    day_element.classList.add("day");
-    day_element.textContent = i + 1;
-    let day_element_date_value = new Date(year, month, i + 1);
-    day_element.dataset.value = day_element_date_value;
+//   for (let i = 0; i < amount_days; i++) {
+//     const day_element = document.createElement("div");
+//     day_element.classList.add("day");
+//     day_element.textContent = i + 1;
+//     let day_element_date_value = new Date(year, month, i + 1);
+//     day_element.dataset.value = day_element_date_value;
 
-    let yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    if (day_element_date_value < yesterday) {
-      day_element.classList.add("unavailable");
-    }
+//     let yesterday = new Date();
+//     yesterday.setDate(yesterday.getDate() - 1);
+//     if (day_element_date_value < yesterday) {
+//       day_element.classList.add("unavailable");
+//     }
 
-    if (!day_element.classList.contains("unavailable")) {
-      day_element.addEventListener("click", () => {
-        selectedDate = new Date(year, month, i + 1);
-        selectedDay = i + 1;
-        selectedMonth = month;
-        selectedYear = year;
+//     if (!day_element.classList.contains("unavailable")) {
+//       day_element.addEventListener("click", () => {
+//         selectedDate = new Date(year, month, i + 1);
+//         selectedDay = i + 1;
+//         selectedMonth = month;
+//         selectedYear = year;
 
-        date_picker_element.dataset.value = selectedDate;
+//         date_picker_element.dataset.value = selectedDate;
 
-        if (date_picker_element.dataset.type == "checkout") {
-          // let dropdownTime;
-          dropdownTime.classList.remove("hidden");
-          date_picker_element.classList.remove("invalid");
-          dropdownTime.parentElement
-            .querySelector("label[for=dropdownTime]")
-            .classList.remove("hidden");
-        }
+//         if (date_picker_element.dataset.type == "checkout") {
+//           // let dropdownTime;
+//           dropdownTime.classList.remove("hidden");
+//           date_picker_element.classList.remove("invalid");
+//           dropdownTime.parentElement
+//             .querySelector("label[for=dropdownTime]")
+//             .classList.remove("hidden");
+//         }
 
-        populateDays();
-      });
-    }
+//         populateDays();
+//       });
+//     }
 
-    if (
-      selectedDay == i + 1 &&
-      selectedMonth == month &&
-      selectedYear == year
-    ) {
-      day_element.classList.add("selected");
-    }
+//     if (
+//       selectedDay == i + 1 &&
+//       selectedMonth == month &&
+//       selectedYear == year
+//     ) {
+//       day_element.classList.add("selected");
+//     }
 
-    days_element.appendChild(day_element);
-  }
-}
+//     days_element.appendChild(day_element);
+//   }
+// }
 
-function formatDate(date) {
-  let day = date.getDate();
-  if (day < 10) {
-    day = "0" + day;
-  }
-  let month = date.getMonth() + 1;
-  if (month < 10) {
-    month = "0" + month;
-  }
-  let year = date.getFullYear();
-  return day + " / " + month + " / " + year;
-}
+// function formatDate(date) {
+//   let day = date.getDate();
+//   if (day < 10) {
+//     day = "0" + day;
+//   }
+//   let month = date.getMonth() + 1;
+//   if (month < 10) {
+//     month = "0" + month;
+//   }
+//   let year = date.getFullYear();
+//   return day + " / " + month + " / " + year;
+// }
